a4408636b30585b78b77907735101ad829459b94
David <davidroussel171113@outlook.fr>
2022-02-08 23:57:27 +0100

    Ajout du fichier javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+const ratio = .1;
+const options = {
+    root : null,
+    rootMargin: '0px',
+    threshold : .1
+}
+
+const handleIntersect = (entries, observer) => {
+    entries.forEach(function (entry) {
+        if (entry.intersectionRatio > ratio) {
+            entry.target.classList.add('reveal-img-visible')
+            observer.unobserve(entry.target)
+        }
+    })
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+document.querySelectorAll('.reveal-img').forEach(function (r) {
+    observer.observe(r);
+})
